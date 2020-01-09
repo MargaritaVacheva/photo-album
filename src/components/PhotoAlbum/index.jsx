cf20bbae848c4a962b22b269ea489b8fae5e7fb4
MargaritaVacheva <m.d.vacheva@gmail.com>
2020-01-09 13:32:06 +0200

    add and remove favourite

--- src/components/PhotoAlbum/index.jsx
+++ src/components/PhotoAlbum/index.jsx
@@ -1,37 +1,50 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { addToFavourites, removeFromFavourites } from '../../store/actions'
 
-const PhotoAlbum = ({ albumId, photos, isLoading, error }) => {
+const PhotoAlbum = ({ albumId, photos, isLoading, error, favourites, addToFavourites, removeFromFavourites }) => {
 
     return (
-        <section>
+        <section className="album-gallery">
             <h3>Photos from album: {albumId}</h3>
             {isLoading ?
                 <div>Loading...</div> :
-                <>
+                <div className="album-cards">
                     {error && <p>{error}</p>}
                     {photos
                         .filter(p => p.albumId === albumId)
                         .map(p => {
+                            let isInFavourites = favourites.includes(p.id)
                             return (
                                 <div className="photo-card" key={p.id}>
                                     <img src={p.url} alt={p.title} />
-                                    <span>{p.title}</span>
+                                    <p>{p.title}</p>
+                                    { !isInFavourites ?
+                                        <button onClick={() => addToFavourites(p.id)}>Add to Favourites</button> :
+                                        <button onClick={() => removeFromFavourites(p.id)}>Favourite</button>
+                                    }
                                 </div>
                             )
                         })}
-                </>
-            }
+                </div>}
         </section>
     );
 }
+
 const mapStateToProps = (state) => ({
     albumId: state.albums.currentAlbum,
     photos: state.photos.data,
     isLoading: state.photos.isFetching,
     error: state.photos.error,
+    favourites: state.favourites
+})
+
+const mapDispatchToProps = (dispatch) => ({
+    addToFavourites: (id) => dispatch(addToFavourites(id)),
+    removeFromFavourites: (id) => dispatch(removeFromFavourites(id))
 })
 
 export default connect(
-    mapStateToProps
+    mapStateToProps,
+    mapDispatchToProps
 )(PhotoAlbum);
